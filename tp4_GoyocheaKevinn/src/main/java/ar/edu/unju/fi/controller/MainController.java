3918e89b6a0ddfabf225ce34ad5c7dd35aabf42a
kevin <kevin@LaGoat>
2024-05-28 21:40:03 -0300

    creacion de la rama clases en la que se crean todas las clases utiles

---
+++ tp4_GoyocheaKevinn/src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class MainController {
+	@GetMapping("/inicio")
+	public String getIndexPage() {
+		return "index";
+	}
+}
