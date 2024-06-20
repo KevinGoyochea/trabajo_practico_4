fd5fd0f0c5d84c6fb3ca04f3fef312b75e930e29
kevin <kevin@LaGoat>
2024-06-20 19:49:07 -0300

    Creacion del proyecto

--- tp4_GoyocheaKevinn/src/main/java/ar/edu/unju/fi/controller/MainController.java
+++ tp4_GoyocheaKevinn/src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -2,9 +2,10 @@ package ar.edu.unju.fi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MainController {
 	@GetMapping("/inicio")
-	public String getIndexPage() {
+	public String getIndex() {
 		return "index";
 	}
 }
